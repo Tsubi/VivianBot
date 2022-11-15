366ba99e6eb8cf145e3d052e91bb0c663237f278
Noah Steinberg <noah.steinberg@carleton.ca>
2022-11-14 19:19:12 -0500

    Fixed role removal, and added log_lambdas file to keep longer log messages out of spamming the main code file

--- bot.py
+++ bot.py
@@ -6,6 +6,10 @@ import glob
 from logging import StreamHandler
 from logging.handlers import TimedRotatingFileHandler
 from dotenv import load_dotenv
+from discord.utils import get
+
+# Local file with helpful log message formatters
+from log_lambdas import *
 
 # Logging Configuration----------------------------------------------------
 log = logging.getLogger('VivianBot')
@@ -61,14 +65,15 @@ mod_role_id = int(os.getenv('MOD_ROLE_ID'))
 testing_channel_id = int(os.getenv('TESTING_CHANNEL_ID'))
 
 # Global variables to hold objects relating to configuration
-# Not populated until bot has had a change to run 
+# Not populated until bot has had a chance to run 
 guild = None
 mod_role = None
 # ----------------------------------------------------
 
 
 # Connect to discord API
-client = discord.Client(intents=discord.Intents.all())
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
     
 
 # Post-login startup sequence
@@ -84,6 +89,9 @@ async def on_ready():
 # message sent event
 @client.event
 async def on_message(message):
+
+
+
   try:
     if message.author == client.user:
         return
@@ -112,25 +120,9 @@ async def on_message(message):
     #     Content: {content}"""
     # )
 
-    # Log generated when a command is detected
-    log_command = lambda command: log.info(
-      f"""Command Recieved: 
-        Author: {author.name}
-        Channel: {channel.name}
-        Command: "{command}" """
-    )
-
-    # Log generated when a 'keyword' is detected
-    log_automated_response = lambda keyword: log.info(
-      f"""Keyword Recieved: 
-        Author: {author.name}
-        Channel: {channel.name}
-        Keyword(s): {keyword} """
-    )
-
     # Vivian Commands----------------------------------------------------
     if command in vivAllCommands:
-      log_command(command)
+      log_command(message, command)
 
       # Global User Commands----------------------------------------------------
       if command in vivGlobalCommands:
@@ -147,11 +139,11 @@ async def on_message(message):
 
         #!raffle <title> <emote>
         if content_lower.startswith(vivModCommands[0]):
-          log.error("NOT IMPLEMENTED")
+          raise NotImplementedError()
 
         #!endraffle <title>
         elif content_lower.startswith(vivModCommands[0]):
-          log.error("NOT IMPLEMENTED")
+          raise NotImplementedError()
       # ----------------------------------------------------
 
     # ----------------------------------------------------
@@ -161,7 +153,7 @@ async def on_message(message):
       # vivAttention----------------------------------------------------
       attention = [word for word in vivAttention if word in content_lower]
       if any(attention):
-        log_automated_response(attention)
+        log_automated_response(message, attention)
         response = random.choice(vivBirdNoises)
         
       # ----------------------------------------------------
@@ -176,6 +168,7 @@ async def on_message(message):
     if response != None:
       await message.channel.send(response)
   except Exception as e:
+    log_message_error(message, e)
     log.error(f"Encountered error {e} while processing a message.")
     log.error(f"Message: {message.content} \n Author {message.author.name}")
 
@@ -184,120 +177,127 @@ async def on_message(message):
 async def on_raw_reaction_add(payload):
   try:
     msg_id = payload.message_id
+    emoji_id = payload.emoji.id
+    member = payload.member
+    role = None
 
     # Pronoun Roles----------------------------------------------------
     if msg_id == 802997626995343370:
-      if payload.emoji.id == 802976289379713025:
+      
+      if emoji_id == 802976289379713025:
         # He/Him
         role = discord.utils.get(guild.roles, id=802943259596685332)
-      elif payload.emoji.id == 802976289597947914:
+      elif emoji_id == 802976289597947914:
         # She/Her
         role = discord.utils.get(guild.roles, id=802943355960164372)
-      elif payload.emoji.id == 802976289681834014:
+      elif emoji_id == 802976289681834014:
         # They/Them
         role = discord.utils.get(guild.roles, id=802943394409480202)
       else:
-        log.warn(f"Server member {member.name} reacted to pronoun roles message with invalid emote")
+        log_invalid_emoji(payload, "Pronoun")
     # ----------------------------------------------------
 
     # Game Roles----------------------------------------------------
     elif msg_id == 802997668304650280:
-      if payload.emoji.id == 803000326184894496:
+      if emoji_id == 803000326184894496:
         # Art Games
         role = discord.utils.get(guild.roles, id=802943449404801054)
-      elif payload.emoji.id == 803000326218317844:
+      elif emoji_id == 803000326218317844:
         # OC Questions
         role = discord.utils.get(guild.roles, id=802943523979526174)
-      elif payload.emoji.id == 978373881268146256:
+      elif emoji_id == 978373881268146256:
         # Gamerz
         role = discord.utils.get(guild.roles, id=978375025461719070)
       else:
-        log.warn(f"Server member {member.name} reacted to game roles message with invalid emote")
+        log_invalid_emoji(payload, "Game")
     # ----------------------------------------------------
       
 
     # Stream Roles----------------------------------------------------
     elif msg_id == 931991719136878602:
-      if payload.emoji.id == 845447895758274631:
+      if emoji_id == 845447895758274631:
         # Isabelle Stream notification
         role = discord.utils.get(guild.roles, id=932759248755126303)
-      elif payload.emoji.id == 794017620965720094:
+      elif emoji_id == 794017620965720094:
         # Mark Stream notification
         role = discord.utils.get(guild.roles, id=1024670256381304842)
       else:
-        log.warn(f"Server member {member.name} reacted to stream roles message with invalid emote")
+        log_invalid_emoji(payload, "Stream")
     # ----------------------------------------------------
       
     if role is not None:
       member = payload.member
       if member is not None:
         await member.add_roles(role)
-        log.info(f"Added role {role.name} to server member {member.name}")
+        log_add_role(member, role)
       else:
-        log.info(f"Attempted to add role {role.name}, but couldn't find original user which requested the addition.")
+        log.error(f"Attempted to add a role to a member that doesn't exist???")
   except Exception as e:
-    log.error(f"Encountered error {e} while processing a raw_reaction_add.")
-    log.error(f"Emoji: {payload.emoji.id}\n Member: {payload.member.name}")
+    log_reaction_error(payload, e)
 
 # reaction remove event
 @client.event
 async def on_raw_reaction_remove(payload):
   try:
+    # Refresh guild, to refresh members
+    global guild
+    guild = await client.fetch_guild(payload.guild_id)
+    
     msg_id = payload.message_id
+    emoji_id = payload.emoji.id
+    member = await guild.fetch_member(payload.user_id)
     role = None
 
     # Pronoun Roles----------------------------------------------------
     if msg_id == 802997626995343370:
-      if payload.emoji.id == 802976289379713025:
+      if emoji_id == 802976289379713025:
         #He/Him
         role = discord.utils.get(guild.roles, id=802943259596685332)
-      elif payload.emoji.id == 802976289597947914:
+      elif emoji_id == 802976289597947914:
         #She/Her
         role = discord.utils.get(guild.roles, id=802943355960164372)
       elif payload.emoji.id == 802976289681834014:
         #They/Them
         role = discord.utils.get(guild.roles, id=802943394409480202)
       else:
-        log.warn(f"Server member {member.name} reacted to pronoun roles message with invalid emote")
+        log_invalid_emoji(payload, "Pronoun")
     # ----------------------------------------------------
 
 
     # Game Roles----------------------------------------------------
     elif msg_id == 802997668304650280:
-      if payload.emoji.id == 803000326184894496:
+      if emoji_id == 803000326184894496:
         #Art Games
         role = discord.utils.get(guild.roles, id=802943449404801054)
-      elif payload.emoji.id == 803000326218317844:
+      elif emoji_id == 803000326218317844:
         #OC Questions
         role = discord.utils.get(guild.roles, id=802943523979526174)
-      elif payload.emoji.id == 978373881268146256:
+      elif emoji_id == 978373881268146256:
         #Gamerz
         role = discord.utils.get(guild.roles, id=978375025461719070)
       else:
-        log.warn(f"Server member {member.name} reacted to game roles message with invalid emote")
+        log_invalid_emoji(payload, "Game")
     # ----------------------------------------------------
 
     # Stream Roles----------------------------------------------------
     elif msg_id == 931991719136878602:
-      if payload.emoji.id == 845447895758274631:
+      if emoji_id == 845447895758274631:
         #Isabelle Stream notification
         role = discord.utils.get(guild.roles, id=932759248755126303)
-      elif payload.emoji.id == 794017620965720094:
+      elif emoji_id == 794017620965720094:
         #Mark Stream notification
         role = discord.utils.get(guild.roles, id=1024670256381304842)
       else:
-        log.warn(f"Server member {member.name} reacted to stream roles message with invalid emote")
+        log_invalid_emoji(payload, "Stream")
     #----------------------------------------------------
       
     if role is not None:
-      member = payload.member
       if member is not None:
         await member.remove_roles(role)
-        log.info(f"Removed role {role.name} from server member {member.name}")
+        log_remove_role(member, role)
       else:
-        log.info(f"Attempted to remove role {role.name}, but couldn't find original user which requested the addition.")
+        log.error(f"Attempted to remove a role from a member that could not be found ({payload.user_id})")
   except Exception as e:
-    log.error(f"Encountered error {e} while processing a raw_reaction_remove.")
-    log.error(f"Emoji: {payload.emoji.id}\n Member: {payload.member.name}")
+    log_reaction_error(payload, e)
 
 client.run(client_token)
---
+++ log_lambdas.py
@@ -0,0 +1,55 @@
+import logging
+
+log = logging.getLogger('VivianBot')
+
+# Log generated for a failure during message parsing
+log_message_error = lambda message, error: log.error(
+f"""Message Parsing Failed!:
+    Author: {message.content.author.name} ({message.content.author.id})
+    Channel: {message.channel.name}
+    Error: {error}
+    Message: "{message.content}" ({message.id}) """
+)
+
+# Log generated when a command is detected
+log_command = lambda message, command: log.info(
+    f"""Command Recieved: 
+    Author: {message.author.name} ({message.author.id})
+    Channel: {message.channel.name}
+    Command: "{command}" """
+)
+
+# Log generated when a 'keyword' is detected
+log_automated_response = lambda message, keyword: log.info(
+    f"""Keyword Recieved: 
+    Author: {message.author.name} ({message.author.id})
+    Channel: {message.channel.name}
+    Keyword(s): {keyword} """
+)
+
+log_reaction_error = lambda payload, error: log.error(
+f"""Reaction Parsing Failed!:
+    Author: {payload.member.name} ({payload.member.id})
+    Message: {payload.message_id}
+    Emote: {payload.emoji.id} """
+)
+
+# Invalid Emoji was used on a Role-Add message
+log_invalid_emoji = lambda payload, role_type: log.warn(
+  f"""Invalid Emoji Recieved for Role Change: 
+    Member: {payload.member.name} ({payload.member.id})
+    Role Type: {role_type}
+    Emote: {payload.emoji.id} """
+)
+
+log_add_role = lambda member, role: log.info(
+f"""Role added to member: 
+    Member: {member.name} ({member.id})
+    Role: {role.name} """
+)
+
+log_remove_role = lambda member, role: log.info(
+f"""Role removed from member: 
+    Member: {member.name} ({member.id})
+    Role Id: {role.name} """
+)
